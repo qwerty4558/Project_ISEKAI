ab0c2d034bc38d27e6136e4184b873aa475d0a00
qwerty4558 <30473988+qwerty4558@users.noreply.github.com>
2023-01-08 03:56:55 +0900

    재료(나무캐기), 작업대 작업 기초 작업

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float hor_Move;
+    float ver_Move;
+
     [SerializeField] float moveSpeed = 4f;
     [SerializeField] float rotateSpeed = 30f;
 
     Rigidbody rigidbody;
     Animator animator;
+
+
     /*[SerializeField] float dashSpeed = 7f;*/
 
 
     Vector3 dir = Vector3.zero;
+
+    Vector3 heading;
+
     bool isMove = false;
 
 
@@ -28,29 +37,54 @@ public class PlayerController : MonoBehaviour
     {
         Move();
         Player_Run();
-        
+        PlayerSetAnimations();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Tree")) Debug.Log("Enter Tree");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Tree"))
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                other.GetComponent<Tree>().Mine();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Tree")) Debug.Log("Exit Tree");
     }
 
     private void Move()
     {
-        dir.x = Input.GetAxis("Horizontal");
-        dir.z = Input.GetAxis("Vertical");
-        dir.Normalize();
+
+        hor_Move = Input.GetAxis("Horizontal");
+        ver_Move = Input.GetAxis("Vertical");
+
+        heading = Camera.main.transform.rotation * Vector3.forward;
+        heading.y = 0;
+        heading = heading.normalized;
+
+        dir = heading * Time.deltaTime * ver_Move * moveSpeed;
+        dir += Quaternion.Euler(0, 90, 0) * heading * Time.deltaTime * hor_Move * moveSpeed;
 
         if (dir != Vector3.zero)
         {
             isMove = true;
-            transform.forward = Vector3.Lerp(transform.forward, dir/2, rotateSpeed * Time.deltaTime);
-            animator.SetBool("isWalk", isMove);
+            //transform.forward = Vector3.Lerp(transform.forward, dir, rotateSpeed * Time.deltaTime);            
         }
         else
         {
-            isMove = false;
-            animator.SetBool("isWalk", isMove);
+            isMove = false;            
         }
-
-        rigidbody.MovePosition(this.gameObject.transform.position + dir * moveSpeed * Time.deltaTime);
+        //rigidbody.MovePosition(dir  * Time.deltaTime);
+        transform.Translate(dir);
     }
+
     private void Player_Run()
     {
         if (Input.GetKey(KeyCode.LeftShift) && isMove)
@@ -63,5 +97,9 @@ public class PlayerController : MonoBehaviour
             moveSpeed = 4f;
             animator.SetBool("isJog", false);
         }
+    }
+    private void PlayerSetAnimations()
+    {
+        animator.SetBool("isWalk", isMove);
     }
 }
