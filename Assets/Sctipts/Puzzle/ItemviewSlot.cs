ec4abee07c8beb66e2609420e90dc36abc794e0a
BadToast <ship2042@gmail.com>
2023-04-24 22:12:20 +0900

    Font Fix / Ingredient settings

--- Assets/Sctipts/Puzzle/ItemviewSlot.cs
+++ Assets/Sctipts/Puzzle/ItemviewSlot.cs
@@ -5,12 +5,14 @@ public class ItemviewSlot : MonoBehaviour
 {
     [SerializeField] private Ingredient_Item ItemData;
     [SerializeField] private Image image;
+    [SerializeField] private Image patternImage;
 
 
     public void SetItemData(Ingredient_Item item)
     {
         ItemData = item;
         image.sprite = Resources.Load<Sprite>(item.icon_File_Name);
+        patternImage.sprite = item.itemPatternImage;
     }
 
     public void WriteOnPot()
