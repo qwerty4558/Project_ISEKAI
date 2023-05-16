13837bfb01beadd1f989995e6977380e8f8d4d55
qwerty4558 <qwerty4558@gmail.com>
2023-05-16 18:55:03 +0900

    test

---
+++ Assets/test/KJH Test/Scripts/Settting/Setting.cs
@@ -0,0 +1,62 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour
+{
+    [Header("볼륨 조절")]
+    public AudioMixer masterMixer;
+
+    public Slider masterSlider;
+    public Slider BGMSlider;
+    public Slider SFXSlider;
+
+    [Header("카메라 조절")]
+    public CinemachineFreeLook freelook;
+
+    public void Start()
+    {
+        freelook = FindObjectOfType<CinemachineFreeLook>();
+        masterSlider.value = 1f;
+        BGMSlider.value = -10f;
+        SFXSlider.value = -10f;
+    }
+
+    public void AudioMasterControll()
+    {
+        float soundVolume = masterSlider.value;
+        if (soundVolume == -40f)
+        {
+            masterMixer.SetFloat("Master", -80f);
+        }
+        else masterMixer.SetFloat("Master", soundVolume);
+    }
+
+    public void AudioSFXControll()
+    {
+        float soundVolume = SFXSlider.value;
+        if (soundVolume == -40f)
+        {
+            masterMixer.SetFloat("SFX", -80f);
+        }
+        else masterMixer.SetFloat("SFX", soundVolume);
+    }
+
+    public void AudioBGMControll()
+    {
+        float soundVolume = BGMSlider.value;
+        if (soundVolume == -40f)
+        {
+            masterMixer.SetFloat("BGM", -80f);
+        }
+        else masterMixer.SetFloat("BGM", soundVolume);
+    }
+
+    public void ToggleAudioVolume()
+    {
+        AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+    }
+}
