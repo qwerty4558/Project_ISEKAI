b0eae920df2b0621ae04bf49cf8011176c593a4c
qwerty4558 <30473988+qwerty4558@users.noreply.github.com>
2023-05-26 22:57:50 +0900

    ShopScene Update And UI Hide Setting

--- Assets/Sctipts/ShopScene/ShopScene.cs
+++ Assets/Sctipts/ShopScene/ShopScene.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class ShopScene : MonoBehaviour
 {
     [SerializeField] GameObject _craftPuzzle;
     [SerializeField] GameObject _discrimination;
+    [SerializeField] GameObject _inGameUI;
+    [SerializeField] GameObject _questUI;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scane, LoadSceneMode loadSceneMode)
+    {
+        if(_inGameUI == null && _questUI == null)
+        {
+            _inGameUI = GameObject.FindGameObjectWithTag("InGameUI");
+            _questUI = GameObject.FindGameObjectWithTag("QuestUI");
+        }
     }
 
     // Update is called once per frame
@@ -23,11 +45,15 @@ public class ShopScene : MonoBehaviour
             {
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
+                _inGameUI.SetActive(false);
+                _questUI.SetActive(false);
             }
             else
             {
                 Cursor.lockState= CursorLockMode.Locked;
                 Cursor.visible = false;
+                _inGameUI.SetActive(true);
+                _questUI.SetActive(true);
             }
         }
         
