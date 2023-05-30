79c7163fbbd19b6aa010c656ab581b9d1549e6c0
qwerty4558 <30473988+qwerty4558@users.noreply.github.com>
2023-05-30 22:13:34 +0900

    check before

---
+++ Assets/test/KJH Test/Scripts/DataMoveToScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataMoveToScene
+{
+    public bool isHasAxe;
+    public bool isHasSword;
+    public bool isHasPickaxe;
+}
