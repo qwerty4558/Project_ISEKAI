0f95530c663839d643ebd6c8165db406700ecc9a
qwerty4558 <qwerty4558@gmail.com>
2023-06-08 02:16:16 +0900

    Tutorial Canvas Made

---
+++ Assets/ShopTutorial.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTutorial : MonoBehaviour
+{
+    [SerializeField] GameObject[] pages;
+    [SerializeField] GameObject spaceImage;
+    [SerializeField] float getDelay = 1f;
+    [SerializeField] bool isNext;
+
+    private int currentPageIndex = 0;
+
+    private void Start()
+    {
+        spaceImage.SetActive(false);
+
+        for (int i = 0; i < pages.Length; i++)
+        {
+            for (int j = 0; j < pages[i].transform.childCount; j++)
+            {
+                pages[i].transform.GetChild(j).gameObject.SetActive(false);
+            }
+            pages[i].SetActive(false);
+        }
+        isNext = false;
+        StartCoroutine(IDoTutorial(0));
+    }
+
+    private void Update()
+    {
+        if (isNext)
+        {
+            if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                NextPage();
+            }
+        }
+    }
+
+    private void NextPage()
+    {
+        if (currentPageIndex + 1 < pages.Length)
+        {
+            currentPageIndex++;
+            pages[currentPageIndex - 1].SetActive(false);
+            if (currentPageIndex < pages.Length)
+            {
+                StartCoroutine(IDoTutorial(currentPageIndex));
+            }
+        }
+        else this.gameObject.SetActive(false);
+    }
+
+    IEnumerator IDoTutorial(int index)
+    {
+        spaceImage.SetActive(false);
+        isNext = false;
+        if (index >= 0 && index < pages.Length)
+        {
+            pages[index].SetActive(true);
+            for (int i = 0; i < pages[index].transform.childCount; ++i)
+            {
+                pages[index].transform.GetChild(i).gameObject.SetActive(true);
+                yield return new WaitForSeconds(getDelay);
+            }
+        }
+
+        spaceImage.SetActive(true);
+        isNext = true;
+        yield return null;
+    }
+}
