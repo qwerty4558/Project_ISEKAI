690458de1388c6fd6c62cee8d1350634d4fcd79e
qwerty4558 <30473988+qwerty4558@users.noreply.github.com>
2023-06-24 06:54:02 +0900

    Spawn magic stone range, create the magic stone

--- Assets/test/KJH Test/Scripts/BOSS_Witch/BOSS_Witch.cs
+++ Assets/test/KJH Test/Scripts/BOSS_Witch/BOSS_Witch.cs
@@ -7,21 +7,25 @@ public class BOSS_Witch : MonoBehaviour
     PlayerController player;
     public List<WitchState> states = new List<WitchState>();
 
-    [SerializeField] float maxHP;
+    [SerializeField] float maxHP = 100f;
     [SerializeField] float currentHP;
     [SerializeField] float attackRange;
     [SerializeField] float attackSpeed;
     [SerializeField] float attackTime;
+    [SerializeField] float faintTimeDelay;
 
-    [SerializeField] float moveRidius;
-    [SerializeField] float moveSpeed;
-    [SerializeField] float angle = 0f;
+    [SerializeField] float spawnStoneRidius = 10f;
+
 
 
     [SerializeField] bool isAction;
     [SerializeField] bool isHit;
+    [SerializeField] bool isFaint;
     [SerializeField] bool isAttack;
     [SerializeField] bool isBossStart;
+    [SerializeField] bool hasSpawnMagicStone;
+    [SerializeField]
+    bool[] magicStoneBreak;
 
     [SerializeField] GameObject bossDefPos;
 
@@ -34,10 +38,11 @@ public class BOSS_Witch : MonoBehaviour
 
     [SerializeField] float targetHeight;
     [SerializeField] float upPositionDuration;
-    
+    float _faintTime = 10f;
 
     [SerializeField]
     GameObject[] magicStone;
+    [SerializeField] int stoneCount;
 
     [SerializeField] Animator animator;
     [SerializeField] Outline outline;
@@ -45,7 +50,7 @@ public class BOSS_Witch : MonoBehaviour
     [SerializeField] UnityEngine.UI.Image hpBar;
 
 
-    void Start()
+    void Awake()
     {
         InitWitchBoss();
     }
@@ -59,58 +64,146 @@ public class BOSS_Witch : MonoBehaviour
         currentHP = maxHP;
         animator = GetComponent<Animator>();
         outline = GetComponent<Outline>();
-
-        magicStone = new GameObject[magicStone.Length];
-
+        hasSpawnMagicStone = false;
+        stoneCount = magicStone.Length;
+        magicStoneBreak = new bool[stoneCount];
         WitchBossStart();
     }
 
     private void WitchBossStart()
     {
         StartCoroutine(CO_Flying_Witch());
+        isBossStart = true;
     }
 
     IEnumerator CO_Flying_Witch()
     {
-        float _startY = transform.position.y;
+        if (!isFaint)
+        {
+            float _startY = transform.position.y;
 
+            float elapsedTime = 0f;
+
+
+            while (elapsedTime < upPositionDuration)
+            {
+                float newY = Mathf.Lerp(_startY, targetHeight, elapsedTime / upPositionDuration);
+
+                transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = new Vector3(transform.position.x, targetHeight, transform.position.z);
+            yield return null;
+        }
+        
+
+    }
+
+    IEnumerator CO_Drop_Witch()
+    {
+     
+        float startY = transform.position.y;
         float elapsedTime = 0f;
 
-
-        while(elapsedTime < upPositionDuration)
+        while (elapsedTime < upPositionDuration)
         {
-            float newY = Mathf.Lerp(_startY, targetHeight, elapsedTime / upPositionDuration);
-
+            float newY = Mathf.Lerp(startY, 0f, elapsedTime / upPositionDuration);
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
-        transform.position = new Vector3(transform.position.x, targetHeight, transform.position.z);
-        yield return null;
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        
+        yield return new WaitForSeconds(10f);
+        isFaint = false;
+        StartCoroutine(CO_Flying_Witch());
     }
+
+    private void MagicStoneSpawner()
+    {
+        float groundHeight = 0f;
+        float minDistance = 5f; // 오브젝트들 간의 최소 거리
+        groundHeight -= midasTree.transform.position.y;
+        for (int i = 0; i < stoneCount; ++i)
+        {
+            bool isValidPosition = false;
+            Vector3 spawnPos = Vector3.zero;
+            Quaternion spawnRot = Quaternion.identity;
+
+            // 겹치지 않는 위치 찾기
+            while (!isValidPosition)
+            {
+                float randomAngle = Random.Range(0f, 180f); // 0도부터 180도 사이의 랜덤한 각도
+                float angle = randomAngle * Mathf.Deg2Rad;
+                float x = Mathf.Cos(angle) * spawnStoneRidius;
+                float z = Mathf.Sin(angle) * spawnStoneRidius;
+                spawnPos = midasTree.transform.position + new Vector3(x, groundHeight, z);
+
+                isValidPosition = true; // 초기 값은 유효한 위치라고 가정
+
+                // 이미 생성된 오브젝트와 거리 비교
+                for (int j = 0; j < i; j++)
+                {
+                    if (Vector3.Distance(spawnPos, magicStone[j].transform.position) < minDistance)
+                    {
+                        isValidPosition = false;
+                        break;
+                    }
+                }
+            }
+
+            magicStoneBreak[i] = true;
+            GameObject spawnObj = Instantiate(magicStone[i], spawnPos, spawnRot);
+
+            
+        }
+        hasSpawnMagicStone = true;
+    }
+
+
+
+    public void StoneBreakCheck(int index)
+    {
+        magicStoneBreak[index] = false;
+        
+    }
+
+    private bool CheckAllStoneBreak()
+    {
+        foreach (bool stoneBreak in magicStoneBreak)
+        {
+            if (stoneBreak) return false;
+        }
+
+        return true;
+    }
+
 
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        
         if (isBossStart)
         {
-            MoveWitch();
+            
+            if (!isFaint && !hasSpawnMagicStone)
+            {
+                MagicStoneSpawner();        
+            }
+            else
+            {
+                
+                StartCoroutine(CO_Drop_Witch());
+            }
+            isFaint = CheckAllStoneBreak();
         }
+       
     }
 
-    private void MoveWitch()
-    {
-        Vector3 center = midasTree.transform.position;
-
-        Vector3 playerToCenter = player.transform.position - midasTree.transform.position;
-
-        Vector3 lomitVector = Vector3.ClampMagnitude(playerToCenter, moveRidius);
-
-        Vector3 newPos = center + lomitVector;
-
-        transform.position = newPos;
-    }
+  
 }
---
+++ Assets/test/KJH Test/Scripts/BOSS_Witch/MagicStoen.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicStoen : Enemy
+{
+    public int stoneIndex;
+    public BOSS_Witch _witch;
+    protected override void Awake()
+    {
+        base.Awake();
+        outputName = "¸¶·Â¼®";
+        _witch = FindObjectOfType<BOSS_Witch>();
+    }
+
+    protected override void Update()
+    {
+        if (currentHp > 0) TargetCheck();
+    }
+
+    protected override void OnEnable()
+    {
+        Respawn();
+    }
+
+    protected override void Respawn()
+    {
+        currentHp = maxHp;
+    }
+
+    protected override void GetDamage(float damage)
+    {
+        currentHp -= damage;
+        player.OtherCheck(this);
+        player.TargetOutline(this.outline);
+        if(currentHp < 0)
+        {
+            player.IsTarget = false;
+            EnemyDead();
+        }
+    }
+
+    protected override void EnemyDead()
+    {
+        _witch.StoneBreakCheck(stoneIndex);
+        Destroy(this.gameObject);
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("AttackCol"))
+        {
+            if (!other.GetComponent<ActiveAttackCol>().CompareActionType(typeof(Action_Sword))) return;
+
+            float tempDamage = other.GetComponent<ActiveAttackCol>().LinkDamage;
+
+            //hitParticles.Play();
+            hitAnimation.DORestartById("HIT");
+            GetDamage(tempDamage);
+        }
+    }
+}
